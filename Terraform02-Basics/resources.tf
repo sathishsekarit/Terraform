616e9b97ca6728a147b2613faa499098998e5ecf
Sathish Sekar <sathishsekarit@gmail.com>
2026-03-18 23:30:12 +0530

    Learning doc, 02 file

--- Terraform02-Basics/resources.tf
+++ Terraform02-Basics/resources.tf
@@ -1,3 +1,4 @@
+# Variable declaration is one of the methods we can pass value for the variables.
 variable "iam_user_name_prefix" {
   type    = string
   default = "sathishsekarit_iam_user"
@@ -5,6 +6,8 @@ variable "iam_user_name_prefix" {
 
 
 # Creating a iam user
+# We can also export the variable value for the resource usage.
+# Ex: export TF_VAR_iam_user_name_prefix=sathishsekarit_iam_user
 resource "aws_iam_user" "sathishsekarit_iam_internal_users" {
   count = 1
   name  = "${var.iam_user_name_prefix}_${count.index}"
